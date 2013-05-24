3c58c4ec1ef481d27651738121c229c82aa641ee
raul <gaston@raul-Toshiba-G470.(none)>
2013-05-24 18:36:01 -0300

    server

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -10,9 +10,22 @@ import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static spark.Spark.*;
+import spark.*;
+
+
 public class Inmo {
 
-    public static void main(String[] args) {
+	    public static void main(String[] args) {
         Base.open(DB.driver, DB.url, DB.user, DB.password);
+    	
+    	get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }//para ver el hello world en el buscador pongan http://localhost:4567/hello
+      	});
+
+
     }
 }
